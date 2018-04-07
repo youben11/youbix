bfddd75231732abd8dff82fe4832e69dcc1fd03d
youben11 <ayouben9@gmail.com>
2018-04-07 14:05:16 +0100

    adding signature and some comment

--- kernel.asm
+++ kernel.asm
@@ -7,20 +7,21 @@ section .text
 
 ;multiboot specefication
 align 4
-dd 0x1BADB002               ;magic
-dd 0x00                     ;flags
-dd - (0x1BADB002 + 0x00)    ;checksum
+dd 0x1BADB002                  ;magic
+dd 0x00                        ;flags
+dd - (0x1BADB002 + 0x00)       ;checksum
 
 start:
-	cli                        ; no interrupt
+	cli                          ; no interrupt
 	mov esp, stack_start
 	call kmain
-	hlt                        ; no interrupt + hlt = cpu sleep
+	hlt                          ; no interrupt + hlt = cpu sleep
 
 section .data
+db "youbix kernel", 0          ;like a signature for the kernel
 
 section .bss
 
 stack_end:
-resb 8192
+resb 8192                      ;some space for the stack (8KB), you can add more
 stack_start:
