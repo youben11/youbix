bee4750a0406720e4e5d4304f3d3901093e0830b
youben11 <ayouben9@gmail.com>
2018-04-07 18:26:07 +0100

    reading and printing keystroke

--- keyboard.c
+++ keyboard.c
@@ -1,3 +1,5 @@
+#include "keycode.h"
+
 #define KEYBOARD_DATA 0x60
 #define KEYBOARD_STATUS 0x64
 
@@ -5,13 +7,20 @@ extern void prints_at(char* str, char color, int pos);
 extern void clear_screen(char color);
 extern char read_from_port(short port);
 extern void write_to_port(short port, char value);
+extern int cur_pos;
 
 void keyboard_handler(){
-  //write_to_port(0x20,0x20);//EOI
+  write_to_port(0x20,0x20);//EOI
+  unsigned char status = read_from_port(KEYBOARD_STATUS);
+  unsigned char buf[2];
 
-  clear_screen(0x07);
-  prints_at("Key Pressed", 0x07, 0);
-
+  if(status & 1){
+    buf[0] = keycode[read_from_port(KEYBOARD_DATA)-1];
+    if(buf[0] == 0 || buf[0] >= 128)
+      return;
+    buf[1] = '\0';
+    prints_at(buf, 0x07, cur_pos);
+  }
   return;
 }
 
