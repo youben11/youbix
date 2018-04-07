a363bed0238eef6d8c4d000d3f8ae290ea83ed25
youben11 <ayouben9@gmail.com>
2018-04-07 15:09:00 +0100

    polling keycode

--- kernel.c
+++ kernel.c
@@ -1,5 +1,15 @@
+extern char read_from_port(short port);
+extern void write_to_port(short port, char value);
+extern void pic_remap(char base_pic1, char base_pic2);
+extern void set_imr_pic1(char mask);
+extern void set_imr_pic2(char mask);
+extern void _load_idt();
+extern void idt_add(unsigned char interrupt,unsigned short off_higher, unsigned short off_lower, unsigned short selector, unsigned char type_attr, unsigned char zero);
+extern void keyboard_handler_call();
+
 void clear_screen(char color);
 void prints_at(char* str, char color, int pos);
+void polling_keycode();
 
 int cur_pos = 0; // cursor position
 char* video_mem = (char*) 0xb8000; // video memory
@@ -9,8 +19,23 @@ void kmain(void){
 
   clear_screen(0x07);
   prints_at(message, 0x0f, 0);
+  polling_keycode();
 
   return;
+}
+
+void polling_keycode(){
+    char* poll = "polling... keycode:    ";
+    clear_screen(0x07);
+
+    while(1){
+        unsigned char c = read_from_port(0x60);
+        poll[20] = 48 + (c / 100);
+        poll[21] = 48 + ((c%100) / 10);
+        poll[22] = 48 + (c % 10);
+        prints_at(poll, 0x0f, 0);
+    }
+    return;
 }
 
 void prints_at(char* str, char color, int pos){
